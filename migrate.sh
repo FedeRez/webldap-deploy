4d13d6356ca13b97dab410a2d1e327d576a21db3
TsCl <pmontagnier@gmail.com>
2013-07-20 14:47:45 +0200

    Fixed missing -f option

--- migrate.sh
+++ migrate.sh
@@ -4,7 +4,7 @@
 ldapmodify -Y EXTERNAL -H ldapi:/// -f ./migrations/dit/base.ldif
 
 # Migrates the tree
-ldapadd -D "cn=admin,dc=federez,dc=net" -W -H ldapi:/// ./migrations/dit/organizational_units.ldif
+ldapadd -D "cn=admin,dc=federez,dc=net" -W -H ldapi:/// -f ./migrations/dit/organizational_units.ldif
 
 # Migrates overlays
 ldapmodify -Y EXTERNAL -H ldapi:/// -f ./migrations/overlays/memberof.ldif
@@ -18,7 +18,7 @@ ldapmodify -Y EXTERNAL -H ldapi:/// -f ./migrations/overlays/dynlist.ldif
 # File can be found at /etc/ldap/schema/ppolicy.ldif (Debian Wheezy)
 ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/ldap/schema/ppolicy.ldif
 ldapmodify -Y EXTERNAL -H ldapi:/// -f ./migrations/overlays/ppolicy.ldif
-ldapmodify -D "cn=admin,dc=federez,dc=net" -W -H ldapi:/// ./migrations/overlays/ppolicy_policies.ldif
+ldapmodify -D "cn=admin,dc=federez,dc=net" -W -H ldapi:/// -f ./migrations/overlays/ppolicy_policies.ldif
 
 ldapmodify -Y EXTERNAL -H ldapi:/// -f ./migrations/overlays/unique.ldif
 ldapmodify -Y EXTERNAL -H ldapi:/// -f ./migrations/overlays/refint.ldif
