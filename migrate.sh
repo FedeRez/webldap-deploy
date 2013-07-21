05e2792a23c3556837410b361a5fb3ac46465580
Bertrand Bonnefoy-Claudet <bertrandbc@gmail.com>
2013-07-21 16:53:36 +0200

    Update scripts to make building practical
    Works on a fresh debian install with sudo.

--- migrate.sh
+++ migrate.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
-# If needed
-ldapmodify -Y EXTERNAL -H ldapi:/// -f ./migrations/dit/base.ldif
+. ./dist/debian
+
+install_ldap
+
+# Uncomment if needed
+#sudo ldapadd -Y EXTERNAL -H ldapi:/// -f ./migrations/dit/base.ldif
 
 # Migrates the tree
 ldapadd -D "cn=admin,dc=federez,dc=net" -W -H ldapi:/// -f ./migrations/dit/organizational_units.ldif
 
 # Migrates overlays
-ldapmodify -Y EXTERNAL -H ldapi:/// -f ./migrations/overlays/memberof.ldif
+sudo ldapmodify -Y EXTERNAL -H ldapi:/// -f ./migrations/overlays/memberof.ldif
 
 # Requires dyngroup.ldif to be installed
 # File can be found at /etc/ldap/schema/dyngroup.ldif (Debian Wheezy)
-ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/ldap/schema/dyngroup.ldif
-ldapmodify -Y EXTERNAL -H ldapi:/// -f ./migrations/overlays/dynlist.ldif
+sudo ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/ldap/schema/dyngroup.ldif
+sudo ldapmodify -Y EXTERNAL -H ldapi:/// -f ./migrations/overlays/dynlist.ldif
 
 # Requires ppolicy.ldif to be installed
 # File can be found at /etc/ldap/schema/ppolicy.ldif (Debian Wheezy)
-ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/ldap/schema/ppolicy.ldif
-ldapmodify -Y EXTERNAL -H ldapi:/// -f ./migrations/overlays/ppolicy.ldif
+sudo ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/ldap/schema/ppolicy.ldif
+sudo ldapmodify -Y EXTERNAL -H ldapi:/// -f ./migrations/overlays/ppolicy.ldif
 ldapmodify -D "cn=admin,dc=federez,dc=net" -W -H ldapi:/// -f ./migrations/overlays/ppolicy_policies.ldif
 
-ldapmodify -Y EXTERNAL -H ldapi:/// -f ./migrations/overlays/unique.ldif
-ldapmodify -Y EXTERNAL -H ldapi:/// -f ./migrations/overlays/refint.ldif
+sudo ldapmodify -Y EXTERNAL -H ldapi:/// -f ./migrations/overlays/unique.ldif
+sudo ldapmodify -Y EXTERNAL -H ldapi:/// -f ./migrations/overlays/refint.ldif
